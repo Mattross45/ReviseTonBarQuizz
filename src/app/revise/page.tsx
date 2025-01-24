7c24667d231e8a9812ab59f8f8d6541a10b2cc84
Matthieu Rossignon <matthieu.rossignon@hotmail.fr>
2025-01-24 15:43:42 +0100

    Add new revise page

---
+++ src/app/revise/page.tsx
@@ -0,0 +1,9 @@
+export default async function Home() {
+  return (
+    <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 items-center sm:items-start">
+        <h1>Revise</h1>
+      </main>
+    </div>
+  );
+}
